4d2f246b2b002d9773330d41e71dc929442c6863
MateoMahieu <mateomahieu01@gmail.com>
2021-09-01 17:10:28 -0300

    Se agregaron elementos a la pantalla (titulo, descripcion,correo, precio y categoria). Se agrego el spinner con las opciones.

--- app/src/main/java/com/example/compraventa/MainActivity.java
+++ app/src/main/java/com/example/compraventa/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.compraventa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner listaCategorias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaCategorias = (Spinner)findViewById(R.id.spinnerCategoria);
+        String[] categorias = {"INDUMENTARIA", "ELECTRONICA", "ENTRETENIMIENTO", "JARDIN", "VEHICULOS", "JUGUETES"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categorias);
+        listaCategorias.setAdapter(adapter);
     }
 }
