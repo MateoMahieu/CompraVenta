1f68d2abd41810a3f86b01689cc706cf8478bfac
MateoMahieu <mateomahieu01@gmail.com>
2021-09-01 19:07:46 -0300

    Todos los elemntos pedidos fueron agregados a la pantalla. Faltan validaciones

--- app/src/main/java/com/example/compraventa/MainActivity.java
+++ app/src/main/java/com/example/compraventa/MainActivity.java
@@ -3,21 +3,48 @@ package com.example.compraventa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner listaCategorias;
+    private SeekBar barra;
+    private TextView mostrarPorcentaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //coloca las categorias en el spinner
+
         listaCategorias = (Spinner)findViewById(R.id.spinnerCategoria);
-        String[] categorias = {"INDUMENTARIA", "ELECTRONICA", "ENTRETENIMIENTO", "JARDIN", "VEHICULOS", "JUGUETES"};
+
+        String[] categorias = getResources().getStringArray(R.array.listaCategorias);
 
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categorias);
         listaCategorias.setAdapter(adapter);
+
+        //esto es para que muestre el % de la barra
+
+        barra  = (SeekBar) findViewById(R.id.seekBarDescuento);
+        mostrarPorcentaje = (TextView)findViewById(R.id.txtPorcentaje);
+
+        barra.setProgress(0);
+        barra.setMax(100);
+        barra.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar,
+                                                  int progress, boolean fromUser) {
+                        mostrarPorcentaje.setText(String.valueOf(progress) + " %");
+                    }
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                });
     }
 }
