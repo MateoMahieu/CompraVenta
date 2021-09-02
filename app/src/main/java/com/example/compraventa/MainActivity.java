8ef48ce9f49f2179627303d4131f696f5e09164e
MateoMahieu <mateomahieu01@gmail.com>
2021-09-02 20:31:21 -0300

    se agregaron algunas validaciones de campos obligatorios vacios Falta agregar mensaje de porcentaje de descuento tiene que ser mayor a cero y que los campos solo contengan letras, numeros, etc

--- app/src/main/java/com/example/compraventa/MainActivity.java
+++ app/src/main/java/com/example/compraventa/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.compraventa;
 import android.support.v4.app.NotificationCompatSideChannelService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView mostrarPorcentaje;
     private Switch ofrecerDescuento;
     private CheckBox checkRetiroEnPersona;
-    private TextView txtDireccion;
+    private EditText txtDireccion;
     private TextView lblDireccion;
     private CheckBox checkTerminosYcondiciones;
     private Button btnPublicar;
+    private EditText txtTitulo;
+    private EditText txtPrecio;
+    private EditText txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +44,17 @@ public class MainActivity extends AppCompatActivity {
 
         //coloca las categorias en el spinner
 
-        listaCategorias = (Spinner)findViewById(R.id.spinnerCategoria);
+        listaCategorias = (Spinner) findViewById(R.id.spinnerCategoria);
 
         String[] categorias = getResources().getStringArray(R.array.listaCategorias);
 
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categorias);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categorias);
         listaCategorias.setAdapter(adapter);
 
         //esto es para que muestre el % de la barra
 
-        barraDescuento  = (SeekBar) findViewById(R.id.seekBarDescuento);
-        mostrarPorcentaje = (TextView)findViewById(R.id.txtPorcentaje);
+        barraDescuento = (SeekBar) findViewById(R.id.seekBarDescuento);
+        mostrarPorcentaje = (TextView) findViewById(R.id.txtPorcentaje);
 
         barraDescuento.setProgress(0);
         barraDescuento.setMax(100);
@@ -54,8 +65,10 @@ public class MainActivity extends AppCompatActivity {
                                                   int progress, boolean fromUser) {
                         mostrarPorcentaje.setText(String.valueOf(progress) + " %");
                     }
+
                     public void onStartTrackingTouch(SeekBar seekBar) {
                     }
+
                     public void onStopTrackingTouch(SeekBar seekBar) {
                     }
                 });
@@ -79,16 +92,16 @@ public class MainActivity extends AppCompatActivity {
         //si se selecciona el check de retiro en persona aparecera el campo para ingresar la direccion
 
         checkRetiroEnPersona = (CheckBox) findViewById(R.id.checkBoxRetiro);
-        txtDireccion = (TextView) findViewById(R.id.txtDireccionRetiro);
+        txtDireccion = (EditText) findViewById(R.id.txtDireccionRetiro);
         lblDireccion = (TextView) findViewById(R.id.lblDireccionDeRetiro);
 
         checkRetiroEnPersona.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(checkRetiroEnPersona.isChecked()){
+                if (checkRetiroEnPersona.isChecked()) {
                     lblDireccion.setVisibility(View.VISIBLE);
                     txtDireccion.setVisibility(View.VISIBLE);
-                }else{
+                } else {
                     lblDireccion.setVisibility(View.GONE);
                     txtDireccion.setVisibility(View.GONE);
                 }
@@ -103,19 +116,58 @@ public class MainActivity extends AppCompatActivity {
         checkTerminosYcondiciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(checkTerminosYcondiciones.isChecked()){
+                if (checkTerminosYcondiciones.isChecked()) {
                     btnPublicar.setEnabled(true);
-                }else{
+                } else {
                     btnPublicar.setEnabled(false);
 
                 }
             }
         });
 
-        //si presiona publicar
+        //si presiona publicar valida que los datos sean ingresados
 
+        txtTitulo = (EditText) findViewById(R.id.txtTitulo);
+        txtPrecio = (EditText) findViewById(R.id.txtPrecio);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+
+        btnPublicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //validaciones campos obligaotiros vacios
+
+                if(txtTitulo.getText().toString().isEmpty()){
+                txtTitulo.setError("Campo obligatorio");
+                }
+                if(checkRetiroEnPersona.isChecked() && (txtDireccion.getText().toString().isEmpty())){
+                    txtDireccion.setError("Campo obligatorio");
+                }
+                if(listaCategorias.getSelectedItemId() == 0){
+                    Toast.makeText(getApplicationContext(), "Error: La categoria es un campo obligatorio", Toast.LENGTH_LONG).show();
+                }
+                if(txtPrecio.getText().toString().isEmpty()){
+                    txtPrecio.setError("Campo obligatorio");
+                }else{
+                    if(Integer.parseInt(txtPrecio.getText().toString()) == 0){
+                        txtPrecio.setError("El precio del producto debe ser mayor a 0");
+                    }
+                }
+                if(!txtEmail.getText().toString().isEmpty()){
+                    if(!validarEmail(txtEmail.getText().toString())){
+                        txtEmail.setError("Email no valido");
+                    }
+                }
+            }
+        });
 
     }
 
+    private boolean validarEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
+
 }
+
 
